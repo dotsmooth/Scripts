c720e0ae0e22aede3cc800b38e94acf02feeee6d
RVSmooth <itzsubhashis@proton.me>
2024-11-12 17:28:12 +0000

    Add packages and wallpaper installation

--- twms.sh
+++ twms.sh
@@ -10,6 +10,6 @@ donemsg
 
 emagenta Installing Hyprland and its dependencies....
 $SPS hyprland waybar hyprcursors hyprutils hyprland-scanner xdg-desktop-portal-hyprland xdg-desktop-portal-wlr \
-	grim slurp hyprpicker wl-clipboard qt5-wayland qt6-wayland libdrm pixman libxkbcommon libxml2 llvm \
+	grim slurp hyprpicker hyprpaper wl-clipboard qt5-wayland qt6-wayland libdrm pixman libxkbcommon libxml2 llvm \
 	libpng gegl mtdev xorg-xwayland 
 donemsg
