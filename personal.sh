f596bbbc960c9e0edf5e14e31c312bbfebc24619
RVSmooth <riveeks.smooth@gmail.com>
2024-11-13 22:04:07 +0530

    feat: Setup bare git repo infos

--- personal.sh
+++ personal.sh
@@ -9,4 +9,9 @@ git config --global user.name "RVSmooth"
 git config --global user.email "riveeks.smooth@gmail.com"
 git config --global core.editor "nvim"
 donemsg
-	
+
+config remote add origin git@gitlab.com:dotsmooth/dotfiles.git
+git remote add github git@github.com:RVSmooth/dotfiles.git
+config remote set-url --add --push origin git@github.com:RVSmooth/dotfiles.git
+config remote set-url --add --push origin git@gitlab.com:dotsmooth/dotfiles.git
+git remote set-url --add --push origin git@gitlab.com:dotsmooth/dotfiles.git
